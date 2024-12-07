8fe23b2d62d5aa986bfda948bdf065bdae405489
gaotianchi <6159984@gmail.com>
2024-12-07 12:36:25 +0800

    修改 ClientDao 的格式化方式

--- src/main/java/com/gaotianchi/auth/dto/ClientDto.java
+++ src/main/java/com/gaotianchi/auth/dto/ClientDto.java
@@ -20,50 +20,122 @@ import java.util.Set;
 @Data
 public class ClientDto {
 
-    @NotBlank(message = "客户端ID不能为空", groups = CreateClient.class)
-    @Size(max = 255, message = "客户端ID长度不能超过255", groups = CreateClient.class)
+    @NotBlank(
+            groups = CreateClient.class,
+            message = "客户端ID不能为空"
+    )
+    @Size(
+            groups = CreateClient.class,
+            max = 255, message = "客户端ID长度不能超过255"
+    )
     private String clientId;
 
-    @Size(max = 255, message = "客户端密钥长度不能超过255", groups = CreateClient.class)
+
+    @Size(
+            groups = CreateClient.class,
+            max = 255, message = "客户端密钥长度不能超过255"
+    )
     private String clientSecret;
 
-    @NotBlank(message = "客户端名称不能为空", groups = CreateClient.class)
-    @Size(max = 255, message = "客户端名称长度不能超过255", groups = {CreateClient.class, UpdateClientDetails.class})
+
+    @NotBlank(
+            groups = CreateClient.class,
+            message = "客户端名称不能为空"
+    )
+    @Size(
+            groups = {CreateClient.class, UpdateClientDetails.class},
+            max = 255, message = "客户端名称长度不能超过255"
+    )
     private String clientName;
 
-    @Builder.Default
-    private Date clientIdIssuedAt = new Date();
-    @NotNull(message = "授权类型不能为空", groups = CreateClient.class)
-    private Set<@NotBlank(message = "授权类型不能为空", groups = ClientDto.class) String> authorizationGrantTypes;
+
+    private Date clientIdIssuedAt;
+
+
+    @NotNull(
+            groups = CreateClient.class,
+            message = "授权类型不能为空"
+    )
+    private Set<
+            @NotBlank(
+                    groups = ClientDto.class,
+                    message = "授权类型不能为空"
+            ) String
+            > authorizationGrantTypes;
+
 
     private Date clientSecretExpiresAt;
-    @Size(max = 1000, message = "重定向URI列表长度不能超过1000", groups = CreateClient.class)
-    private Set<@NotBlank(message = "重定向URI不能为空", groups = ClientDto.class) String> redirectUris;
-    @Size(max = 1000, message = "注销后重定向URI列表长度不能超过1000", groups = CreateClient.class)
-    private Set<@NotBlank(message = "注销后重定向URI不能为空", groups = ClientDto.class) String> postLogoutRedirectUris;
-    @NotNull(message = "作用域不能为空", groups = CreateClient.class)
-    private Set<@NotBlank(message = "作用域不能为空", groups = ClientDto.class) String> scopes;
-    @NotNull(message = "客户端认证方式不能为空", groups = CreateClient.class)
-    private Set<@NotBlank(message = "认证方式不能为空", groups = ClientDto.class) String> clientAuthenticationMethods;
+
+
+    @Size(
+            groups = CreateClient.class,
+            max = 1000,
+            message = "重定向URI列表长度不能超过1000"
+    )
+    private Set<
+            @NotBlank(
+                    message = "重定向URI不能为空",
+                    groups = ClientDto.class
+            ) String
+            > redirectUris;
+
+
+    @Size(
+            max = 1000,
+            message = "注销后重定向URI列表长度不能超过1000",
+            groups = CreateClient.class
+    )
+    private Set<
+            @NotBlank(
+                    message = "注销后重定向URI不能为空",
+                    groups = ClientDto.class
+            ) String
+            > postLogoutRedirectUris;
+
+
+    @NotNull(
+            message = "作用域不能为空",
+            groups = CreateClient.class
+    )
+    private Set<
+            @NotBlank(
+                    message = "作用域不能为空",
+                    groups = ClientDto.class
+            ) String
+            > scopes;
+
+
+    @NotNull(
+            message = "客户端认证方式不能为空",
+            groups = CreateClient.class
+    )
+    private Set<
+            @NotBlank(
+                    message = "认证方式不能为空",
+                    groups = ClientDto.class
+            ) String
+            > clientAuthenticationMethods;
+
+
+    private Integer requireProofKey;
+
+
+    private Integer requireAuthorizationConsent;
+
+
+    private Integer accessTokenTimeLive;
+
+
+    private Integer refreshTokenTimeToLive;
+
+
+    private Integer reuseRefreshTokens;
+
 
     public interface CreateClient extends Default {
     }
 
+
     public interface UpdateClientDetails extends Default {
     }
-
-    @Builder.Default
-    private Integer requireProofKey = 1;
-
-    @Builder.Default
-    private Integer requireAuthorizationConsent = 0;
-
-    @Builder.Default
-    private Integer accessTokenTimeLive = 1800;
-
-    @Builder.Default
-    private Integer refreshTokenTimeToLive = 302400;
-
-    @Builder.Default
-    private Integer reuseRefreshTokens = 0;
 }
