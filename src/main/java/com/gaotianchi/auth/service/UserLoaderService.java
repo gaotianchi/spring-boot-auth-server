b7e106c95b160fb2f16cd430556607bf1187a058
gaotianchi <6159984@gmail.com>
2024-12-18 16:18:19 +0800

    Add service method to get paging user list.

--- src/main/java/com/gaotianchi/auth/service/UserLoaderService.java
+++ src/main/java/com/gaotianchi/auth/service/UserLoaderService.java
@@ -1,6 +1,8 @@
 package com.gaotianchi.auth.service;
 
 import com.gaotianchi.auth.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 /**
  * @author gaotianchi
@@ -10,4 +12,6 @@ public interface UserLoaderService {
     String loadCurrentLoggedInUsername();
 
     User loadCurrentLoggedInUser();
+
+    Page<User> getUsersByPage(User user, PageRequest pageRequest);
 }
--- src/main/java/com/gaotianchi/auth/service/impl/UserLoaderServiceImpl.java
+++ src/main/java/com/gaotianchi/auth/service/impl/UserLoaderServiceImpl.java
@@ -3,8 +3,13 @@ package com.gaotianchi.auth.service.impl;
 import com.gaotianchi.auth.dao.UserDao;
 import com.gaotianchi.auth.entity.User;
 import com.gaotianchi.auth.service.UserLoaderService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Get user information from SecurityContextHolder.
@@ -33,5 +38,12 @@ public class UserLoaderServiceImpl implements UserLoaderService {
             throw new RuntimeException("User not found");
         }
         return user;
+    }
+
+    @Override
+    public Page<User> getUsersByPage(User user, PageRequest pageRequest) {
+        long total = userDao.countByUser(user);
+        List<User> users = userDao.selectUsersByPage(user, pageRequest);
+        return new PageImpl<>(users, pageRequest, total);
     }
 }
