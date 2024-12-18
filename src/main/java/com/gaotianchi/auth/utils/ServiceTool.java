8114697742b7ac8e80ae39865bf005228b7fb6fe
gaotianchi <6159984@gmail.com>
2024-12-18 09:01:34 +0800

    Add service tool: getCurrentLoggedInUsername to Get the current logged-in username

--- src/main/java/com/gaotianchi/auth/utils/ServiceTool.java
+++ src/main/java/com/gaotianchi/auth/utils/ServiceTool.java
@@ -1,10 +1,13 @@
 package com.gaotianchi.auth.utils;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+
 /**
  * @author gaotianchi
  * @since 2024/12/16 16:20
  **/
 public class ServiceTool {
+
     /**
      * Generate a random four-digit number
      *
@@ -14,5 +17,16 @@ public class ServiceTool {
      **/
     public static int generateVerificationCode() {
         return (int) (Math.random() * 9000) + 1000;
+    }
+
+    /**
+     * Get the current logged-in username
+     *
+     * @return java.lang.String
+     * @author gaotianchi
+     * @since 2024/12/18 8:46
+     **/
+    public static String getCurrentLoggedInUsername() {
+        return SecurityContextHolder.getContext().getAuthentication().getName();
     }
 }
